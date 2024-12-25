fb0cf217885b58f1482e22cbe0b10dbb0ed75d0e
Sultanulia <sultanaulia237@gmail.com>
2024-12-25 15:25:49 +0700

    WED.25.DEC
    update revisian: FAQ, hover, effect, product/detail product

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
     content: ['./index.html'],
     theme: {
-      extend: {},
+      extend: {
+        backgroundImage: {
+          'Service-Section': "url('https://i.pinimg.com/736x/e6/35/e9/e635e9e5d5224db5491230e727707985.jpg')",
+          backdropGrayscale: {
+            50:'.5'
+          }
+        }
+      },
     },
     plugins: [],
   }
